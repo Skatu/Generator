d6d76f1542fe653ac13209e670cf01305898d3b3
GitHub Action <action@github.com>
2022-04-01 18:37:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/skatu/generator/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/skatu/generator/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.skatu.generator.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.skatu.generator.MyBundle
 
 class MyApplicationService {
 
